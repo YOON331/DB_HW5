05d1ab499cf0e6ed3fd15e8c383a974a1ee7908e
Yoon Byul <yb97pe@gmail.com>
2024-11-21 13:24:01 +0900

    update code

--- rust_to_mysql/src/main.rs
+++ rust_to_mysql/src/main.rs
@@ -18,6 +18,29 @@ fn main() -> Result<()> {
     let pool = Pool::new(database_url).unwrap();
     let mut conn = pool.get_conn().unwrap();
 
+    // table retriever (테이블 조회)
+    println!("\nRetriever table");
+    let result = conn.query_map(
+        "SELECT * FROM Book", // query 문
+        |(bookid, bookname, publisher, price)| Book {
+            bookid,
+            bookname,
+            publisher,
+            price,
+        },
+    )?; // 보낸 쿼리에 대한 결과 mapping
+    for r in result {
+        println!(
+            "{} {} {} {}",
+            r.bookid,
+            r.bookname.unwrap_or("NULL".to_string()),
+            r.publisher.unwrap_or("NULL".to_string()),
+            r.price.unwrap_or_default()
+        );
+        // bookname과 publisher, price는 null 값을 가질 수 있음
+        // rust에서 Int(i32)는 Null 값을 가질 수 없어서 0 으로 mapping
+    }
+
     // 테이블에 데이터 삽입
     conn.query_drop(
         r"insert into Book(bookid, bookname, publisher, price)
